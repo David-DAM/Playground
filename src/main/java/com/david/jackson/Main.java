8b90efad52c07d3965e5d95861e2678f71e72622
David <david11jv@gmail.com>
2024-09-14 12:37:09 +0200

    Add elastic client basic functions

--- src/main/java/com/david/jackson/Main.java
+++ src/main/java/com/david/jackson/Main.java
@@ -1,6 +1,7 @@
 package com.david.jackson;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.javafaker.Faker;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -8,11 +9,13 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.List;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         String PATH_FILE =  "src/main/java/com/david/jackson/products_locations.json";
-
+        //Faker faker = new Faker(Locale.forLanguageTag("es-ES"));
+        //faker.address().latitude()
         FeatureCollection featureCollection = new FeatureCollection();
 
         Feature featureSpain = new Feature();
@@ -22,7 +25,7 @@ public class Main {
 
         Feature featureFrance = new Feature();
         Geometry geometryFrance = new Geometry();
-        geometryFrance.setCoordinates(List.of(-5.2263791347520225,40.366788123826325));
+        geometryFrance.setCoordinates(List.of(2.885396264005152,47.563935417174235));
         featureFrance.setGeometry(geometryFrance);
 
         featureCollection.setFeatures(List.of(featureSpain,featureFrance));
