74297b57524fda66d7c97408303b0eec171a363c
David <david11jv@gmail.com>
2025-11-16 17:06:51 +0100

    add virtual thread executor example

--- src/main/java/com/david/threads/Main.java
+++ src/main/java/com/david/threads/Main.java
@@ -1,13 +1,46 @@
 package com.david.threads;
 
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.*;
 
 public class Main {
     public static void main(String[] args) {
         
     }
 
-    private static void asyncTasks() {
+    private static void executorService() {
+
+        try (ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
+
+            Future<String> task1 = executorService.submit(() -> {
+                System.out.println("Running task 1");
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("Background task 1: " + i);
+                }
+
+                return "Task 1 completed";
+            });
+
+            Future<String> task2 = executorService.submit(() -> {
+                System.out.println("Running task 2");
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("Background task 2: " + i);
+                }
+
+                return "Task 2 completed";
+            });
+
+
+            String task1Result = task1.get();
+            String task2Result = task2.get();
+
+            System.out.println(task1Result + " " + task2Result);
+
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void completableFuture() {
 
         CompletableFuture<Void> task1 = CompletableFuture.runAsync(() -> {
 
